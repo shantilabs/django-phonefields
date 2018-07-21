57ee88cfb3fbd9921af097d8b2960fa81ec5004d
Maxim Oransky <maxim.oransky@gmail.com>
2018-07-21 04:55:54 +0300

    clean() takes 2 positional arguments but 3 were given fix

--- phonefields/__init__.py
+++ phonefields/__init__.py
@@ -331,14 +331,14 @@ class _PhoneValidatorMixin(object):
 
         super(_PhoneValidatorMixin, self).__init__(*args, **kwargs)
 
-    def clean(self, value):
-        return clean_phone(value, **self.validator_options)
-
 
 class FullPhoneFormField(_PhoneValidatorMixin, forms.CharField):
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('max_length', 20)
         super(FullPhoneFormField, self).__init__(*args, **kwargs)
+
+    def clean(self, value):
+        return clean_phone(value, **self.validator_options)
 
 
 # @deconstructible
